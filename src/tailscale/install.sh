ef67a8f3d01a91bf6ba998c91372bd4adac2b09e
Max Lyth <maxlyth@users.noreply.github.com>
2024-08-21 19:53:41 +0100

    Update install.sh

--- src/tailscale/install.sh
+++ src/tailscale/install.sh
@@ -8,7 +8,6 @@ set -euo pipefail
 platform=$(uname -m)
 if [ "$platform" = "x86_64" ]; then
     tailscale_url="https://pkgs.tailscale.com/stable/tailscale_${VERSION}_amd64.tgz"
-elif [ "$platform" = "aarch64" ]; then
 elif [ "$platform" = "aarch64" ] || [ "$platform" = "arm64" ]; then
     tailscale_url="https://pkgs.tailscale.com/stable/tailscale_${VERSION}_arm64.tgz"
 else
